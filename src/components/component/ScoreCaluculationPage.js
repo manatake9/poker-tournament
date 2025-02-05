5401b5c9379b5a98f1e662384e4674ef930c8c8e
sotasage <sotasota.0702@gmail.com>
2025-02-05 14:14:55 +0900

    インデント修正

--- src/components/component/ScoreCaluculationPage.js
+++ src/components/component/ScoreCaluculationPage.js
@@ -6,12 +6,12 @@ import PlayerManagement from './PlayerManagement';
 
 
 const ScoreCaluculationPage = ({ allPlayers, roomId }) => {
-    const [players, setPlayers] = useState(allPlayers);
+  const [players, setPlayers] = useState(allPlayers);
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
-        <ScoreCalculartion players={players} setPlayers={setPlayers} roomId={roomId} />
-        <PlayerManagement players={players} setPlayers={setPlayers} roomId={roomId} />
+      <ScoreCalculartion players={players} setPlayers={setPlayers} roomId={roomId} />
+      <PlayerManagement players={players} setPlayers={setPlayers} roomId={roomId} />
     </div>
   )
 }
