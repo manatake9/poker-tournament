43424aea4aa340c51afc69950cdedc0a84760d49
sotasage <sotasota.0702@gmail.com>
2025-02-05 15:30:44 +0900

    インデントを修正

--- src/components/component/RoomList.js
+++ src/components/component/RoomList.js
@@ -1,38 +1,46 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "../ui/alert-dialog";
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 import { ScrollArea } from "../ui/scroll-area";
 import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
-import Link from 'next/link';
-import { deleteRoom, getAllRooms } from '../../../lib/utils/supabaseFunctions';
+import Link from "next/link";
+import { deleteRoom, getAllRooms } from "../../../lib/utils/supabaseFunctions";
 
-const RoomList = ({ rooms , setRooms}) => {
+const RoomList = ({ rooms, setRooms }) => {
   const handleDelete = async (roomId) => {
     await deleteRoom(roomId);
     const newRooms = await getAllRooms();
     setRooms(newRooms);
-  }
+  };
 
   return (
     <div>
       <ScrollArea className="h-72 w-70 rounded-md border bg-white mb-5">
         <div className="p-4">
-          <h4 className="mb-4 text-base font-medium leading-none">ルーム一覧</h4>
+          <h4 className="mb-4 text-base font-medium leading-none">
+            ルーム一覧
+          </h4>
           {rooms?.map((room) => (
-            <div key={room.room_id} className="text-base p-2 rounded flex justify-between items-center">
-              <Link href={`/room/${room.room_id}`} className="block w-full max-w-sm p-4 bg-white rounded-lg hover:bg-gray-200 transition-all">
+            <div
+              key={room.room_id}
+              className="text-base p-2 rounded flex justify-between items-center"
+            >
+              <Link
+                href={`/room/${room.room_id}`}
+                className="block w-full max-w-sm p-4 bg-white rounded-lg hover:bg-gray-200 transition-all"
+              >
                 {room.room_name}
               </Link>
               <AlertDialog>
@@ -48,7 +56,9 @@ const RoomList = ({ rooms , setRooms}) => {
                   </AlertDialogHeader>
                   <AlertDialogFooter>
                     <AlertDialogCancel>キャンセル</AlertDialogCancel>
-                    <AlertDialogAction onClick={() => handleDelete(room.room_id)}>
+                    <AlertDialogAction
+                      onClick={() => handleDelete(room.room_id)}
+                    >
                       削除する
                     </AlertDialogAction>
                   </AlertDialogFooter>
@@ -60,7 +70,7 @@ const RoomList = ({ rooms , setRooms}) => {
         </div>
       </ScrollArea>
     </div>
-  )
-}
+  );
+};
 
-export default RoomList
+export default RoomList;
--- src/components/component/RoomListPage.js
+++ src/components/component/RoomListPage.js
@@ -1,18 +1,18 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import RoomList from './RoomList';
-import RoomInputForm from './RoomInputForm';
+import React, { useState } from "react";
+import RoomList from "./RoomList";
+import RoomInputForm from "./RoomInputForm";
 
 const RoomListPage = ({ allRooms }) => {
   const [rooms, setRooms] = useState(allRooms);
 
   return (
     <div>
-        <RoomList rooms={rooms} setRooms={setRooms} />
-        <RoomInputForm setRooms={setRooms} />
+      <RoomList rooms={rooms} setRooms={setRooms} />
+      <RoomInputForm setRooms={setRooms} />
     </div>
-  )
-}
+  );
+};
 
-export default RoomListPage
+export default RoomListPage;
